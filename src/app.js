fcb20ce13998b10eb317f1e12d2aacf681dd9be5
Jack Bridger <jacksbridger@gmail.com>
2019-08-22 12:19:03 +0100

    set app.js, modify how port access in index.js Relates #10

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const exphbs = require("express-handlebars");
+const path = require("path");
+const controllers = require("./controllers/index");
+
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.engine(
+  "hbs",
+  exphbs({
+    extname: "hbs",
+    layoutDir: path.join(__dirname, "views", "layouts"),
+    partialsDir: path.join(__dirname, "views", "partials"),
+    defaultLayout: "main"
+  })
+);
 
 module.exports = app;
