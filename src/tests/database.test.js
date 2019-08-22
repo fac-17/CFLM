2ac67f00e3c9949cc493f1325fa2ef0b46dd6e9a
andy <andy.william.mcdonald@gmail.com>
2019-08-22 12:13:01 +0100

    files updated

--- src/tests/database.test.js
+++ src/tests/database.test.js
@@ -1,1 +1,8 @@
 const test = require('tape');
+const runDbBuild = require('../model/database/build');
+
+test ('select all data from users table', (t) => {
+  runDbBuild((err, res) => {
+    t.error(err, 'No error');
+  })
+})
