199ccda567af2aa0c0fc48fe4671c2070c35787e
Gregor <gregorlthomson@sky.com>
2019-08-27 11:13:16 +0100

    Remove console.logs

--- src/controllers/adopt.js
+++ src/controllers/adopt.js
@@ -4,9 +4,7 @@ const animalRequest = require("../model/queries/getData");
 const animals = animalRequest.getAnimals;
 
 exports.get = (req, res) => {
-  console.log({ animals });
   animals((err, result) => {
-    console.log("Result: ", result);
     res.render("adopt", { activePage: { animals: true }, animals: result });
   });
 };
