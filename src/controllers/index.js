8d0fe6350d702ca50ee434476dec6aae984f1681
Gregor <gregorlthomson@sky.com>
2019-08-29 12:53:06 +0100

    Route profile page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const home = require("./home"); // the home we are requiring is the home.js
 const adopt = require("./adopt");
 const error = require("./error");
+const profile = require("./profile");
 
 router.get("/", home.get);
 router.get("/adopt", adopt.get);
+router.get("/profile", profile.get);
 router.use(error.client);
 router.use(error.server);
 
